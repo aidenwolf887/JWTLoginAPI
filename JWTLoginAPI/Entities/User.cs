4bb08000c7a6f7e2076000d7652e830561a7cb5e
aidenwolf887 <madebija098@gmail.com>
2026-05-11 15:23:33 +0700

    Add: User entity to Entities Folder

---
+++ JWTLoginAPI/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JWTLoginAPI.Entities
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
