ddea57108e56374b2f2a5c09d1cb655d9edb11f7
aidenwolf887 <madebija098@gmail.com>
2026-05-11 16:27:47 +0700

    Folder and Code: membuat folder migrasi, membuat database AuthDb, dan mengubah sedikit pada bagian program.cs

---
+++ JWTLoginAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using JWTLoginAPI.Entities;
+
+namespace JWTLoginAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- JWTLoginAPI/Program.cs
+++ JWTLoginAPI/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using JWTLoginAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new Exception("Waduh! Connection String tidak ketemu di User Secrets.");
+}
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
